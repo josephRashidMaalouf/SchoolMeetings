bff6e471d9faef319544901f7f812de2ef1ee36c
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-05-04 22:10:27 +0200

    Refactoring

---
+++ SchoolMeetings/DependencyInjection/InjectViewModels.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Models.ViewModels.Calendars;
+
+namespace SchoolMeetings.Presentation.DependencyInjection;
+
+public static class InjectViewModels
+{
+    public static IServiceCollection AddViewModels(this IServiceCollection services)
+    {
+        services.AddScoped<TeacherCalendarViewModel>();
+
+        return services;
+    }
+}
