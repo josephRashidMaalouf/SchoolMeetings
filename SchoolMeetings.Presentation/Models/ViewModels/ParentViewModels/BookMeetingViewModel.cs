efd598b6f72df455ef66855ab8febf30e33ef48e
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-05-17 08:47:21 +0200

    Working on Book meeting page for parents

---
+++ SchoolMeetings.Presentation/Models/ViewModels/ParentViewModels/BookMeetingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SchoolMeetings.Domain.Entities;
+using SchoolMeetings.Domain.Interfaces;
+
+namespace SchoolMeetings.Presentation.Models.ViewModels.ParentViewModels;
+
+public class BookMeetingViewModel(IMeetingService clientMeetingService)
+{
+    private readonly IMeetingService _clientMeetingService = clientMeetingService;
+
+    public List<Meeting> UnbookedMeetings { get; set; } = [];
+}
